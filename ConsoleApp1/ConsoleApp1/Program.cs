67eaffa836d38f89e0023e80bccf5a8c2ea13f1b
Rock_Crowler <belmaspi@yandex.ru>
2019-07-29 14:15:39 +0300

    Вертикальные и горизонт линии

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,28 +10,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
-            Point p1 = new Point(1, 3,'*') ;
+
+            //Point p1 = new Point(1, 3,'*') ;
             //p1.Draw();
 
-            Point p2 = new Point(4,5,'#');
+            //Point p2 = new Point(4,5,'#');
             //p2.Draw();
 
-            Point p3 = new Point(8, 2, '&');
+            //Point p3 = new Point(8, 2, '&');
 
-            Point p4 = new Point(5, 3, '$');
+            //Point p4 = new Point(5, 3, '$');
 
-            List<Point> plist = new List<Point>();
+            //List<Point> plist = new List<Point>();
 
-        
-            plist.Add(p1);
-            plist.Add(p2);
-            plist.Add(p3);
-            plist.Add(p4);
+            HorizontalLine line = new HorizontalLine(2, 10, 5, '0');
+            line.DrawHorline();
 
-            foreach (Point p in plist) {
-                p.Draw();
-            }
+            VerticalLine Vline = new VerticalLine(5, 10, 2, '1');
+            Vline.DrawVertLine();
+
+
+
+
+          
 
             Console.ReadLine();
 
---
+++ ConsoleApp1/ConsoleApp1/VerticalLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class VerticalLine
+    {
+        List<Point> vLine;
+        public VerticalLine(int yTop, int yDown, int x, char sym) {
+            vLine = new List<Point>();
+            for (int y = yTop; y <= yDown; y++) {
+                Point p = new Point(x, y, sym);
+                vLine.Add(p);
+            }
+        }
+
+        public void DrawVertLine() {
+            foreach (Point p in vLine) {
+                p.Draw();
+            }
+        }
+    }
+}
