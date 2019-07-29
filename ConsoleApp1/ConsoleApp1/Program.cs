d99ecd111ac2a9a04a0791d0076f6019828690fd
Rock_Crowler <belmaspi@yandex.ru>
2019-07-29 11:55:02 +0300

    Упражнение с Списком(List) объектов

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,13 +10,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            
             Point p1 = new Point(1, 3,'*') ;
-            p1.Draw();
+            //p1.Draw();
 
             Point p2 = new Point(4,5,'#');
-            p2.Draw();
+            //p2.Draw();
 
+            Point p3 = new Point(8, 2, '&');
+
+            Point p4 = new Point(5, 3, '$');
+
+            List<Point> plist = new List<Point>();
+
+        
+            plist.Add(p1);
+            plist.Add(p2);
+            plist.Add(p3);
+            plist.Add(p4);
+
+            foreach (Point p in plist) {
+                p.Draw();
+            }
 
             Console.ReadLine();
 
